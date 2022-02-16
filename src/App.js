b6ec5820be53e4fba94690e1232e038b900b91f8
bidesh <paulbidesh2001@gmail.com>
2022-02-16 19:31:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Landing from './components/Landing';
+import Quiz from './components/Quiz';
 
 function App() {
+  
+  const [loading,setloading] = React.useState(true)
+  const [loadQuizPage,setLoadQuizPage] = React.useState(false)
+  function LoadQuiz(){
+    setLoadQuizPage(true)
+  }
+    const [data,setData]=React.useState({
+      // results:[{
+    //   correct_answer:"",
+    //   incorrect_answers:[],
+    //   question:""
+    // },
+    // {
+    //   correct_answer:"",
+    //   incorrect_answers:[],
+    //   question:""
+    // },
+    // {
+    //   correct_answer:"",
+    //   incorrect_answers:[],
+    //   question:""
+    // },
+    // {
+    //   correct_answer:"",
+    //   incorrect_answers:[],
+    //   question:""
+    // },
+    // {
+    //   correct_answer:"",
+    //   incorrect_answers:[],
+    //   question:""
+    // }]
+  })
+    
+  React.useEffect( ()=>{
+    fetch('https://opentdb.com/api.php?amount=5&category=31&difficulty=easy&type=multiple')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(res){
+     setData(res)
+     setloading(false)
+    })
+    .catch(function(err){
+      console.log(err)
+    })},[])
+   
+  if(loading) return (<h1>LOADING...</h1>)
+       if(loadQuizPage) return (<div className="quiz-part">
+       <Quiz data = {data} LoadQuiz = {LoadQuiz} />
+
+       </div>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Landing LoadQuiz = {LoadQuiz}/>
+      
+   </div>
   );
 }
 
